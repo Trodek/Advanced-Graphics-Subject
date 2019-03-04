913023360286aa52ebe5be052567c08a0a5b88b4
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2019-03-04 13:32:08 +0100

    Assimp working without crashing

--- AdvancedGraphics/openglwidget.cpp
+++ AdvancedGraphics/openglwidget.cpp
@@ -39,7 +39,7 @@ QString OpenGLWidget::getOpenGLinfo()
 {
     QString info;
     makeCurrent();
-    info += "OpenGL version:";
+    /*info += "OpenGL version:";
     info += "\n";
     info += glGetString(GL_VERSION);
     info += "\n\n";
@@ -57,11 +57,12 @@ QString OpenGLWidget::getOpenGLinfo()
     info += "\n\n";
 
     info += "OpenGL extensions:";
-    info += "\n";
+    info += "\n";*/
     /*« std::endl; GLint num_extensions; glGetIntegerv(GLNOM_EXTENSIONS, denum_extensions); for (int i = El; i < num_extensions; ++1) const unsigned char *str = glGetStringi(GL_EXTENSIONS, GLuint(i)); std::cout « str « " "; std::cout « std::endl;
     // Information about the surface std::cout « contextO->formatO. std::cout « context()->format(). std::cout « context()->format(). std::cout « context()->format(). std::cout cc context()->format().
     format redBufferSize() « std::endl; greenBufferSize() « std::endt; blueBufferSize() « std::endl; alphaBufferSize() <c std::endl; depthBufferSize() << std::endl;
     */
+    return info;
 }
 
 void OpenGLWidget::finalizeGL()
