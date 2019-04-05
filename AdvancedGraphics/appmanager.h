22c05d42b00f200e0edb4bc35e852b17463c5c6d
Trodek <ibanmas@gmail.com>
2019-04-05 14:01:39 +0200

    AppManage base code

--- AdvancedGraphics/appmanager.h
+++ AdvancedGraphics/appmanager.h
@@ -4,14 +4,26 @@
 #include "mainwindow.h"
 #include "hierarchy.h"
 #include "inspector.h"
+#include "openglwidget.h"
+#include <QObject>
 
-class AppManager
+class AppManager : public QObject
 {
+    Q_OBJECT
 public:
     AppManager();
 
-     static AppManager *Instance();
+    static AppManager *Instance();
 
+    void SetMainWindow(MainWindow* mw);
+    void SetHierarchy(Hierarchy* h);
+    void SetInspector(Inspector* i);
+    void SetOpenGLWidget(OpenGLWidget* oglw);
+
+    MainWindow* GetMainWindow()const;
+    Hierarchy* GetHierarchy()const;
+    Inspector* GetInspector()const;
+    OpenGLWidget* GetOpenGLWidget()const;
 
 private:
      static AppManager* instance;
@@ -19,6 +31,7 @@ private:
      MainWindow* main_window = nullptr;
      Hierarchy* hierarchy = nullptr;
      Inspector* inspector = nullptr;
+     OpenGLWidget* openGL_widget = nullptr;
 };
 
 #endif // APPMANAGER_H
